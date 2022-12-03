fee6c46a0c20f17b6f50ae1faeb39ccfbe9dbc73
Valeria <valeriakuz92@mail.ru>
2022-12-03 18:26:45 +0500

    Сохранение задачи 34

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int[] CreateRandomArray(int N, int start, int end) //готовая функция рандом массива
+{
+int[] array = new int[N];
+for (int i = 0; i < N; i++)
+{
+array[i] = new Random().Next(start, end + 1);
+}
+return array;
+}
+
+void ShowArray(int[] array) //функция вывода массива
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+Console.WriteLine("Введите количество элементов в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the number of start filling array by random numbers by three digit min"); //введите трёхзначное число для нижнего диапазона массива
+int min = Convert.ToInt32(Console.ReadLine());
+
+if (min < 100 || min > 999) //проверка на трёхзначность
+{
+do
+{
+Console.WriteLine ("Wrong number!Enter the number from 100 to 999: ");
+Int32.TryParse(Console.ReadLine(), out min);
+} while (min < 100 || min > 999);
+}
+Console.WriteLine("Enter the number of end filling array by random numbers by three digit max"); //введите трёхзначное число для верхнего диапазона массива
+int max = Convert.ToInt32(Console.ReadLine());
+
+if (max < 100 || max > 999) //проверка на трёхзначность
+{
+do
+{
+Console.WriteLine ("Wrong number!Enter the number from 100 to 999: ");
+Int32.TryParse(Console.ReadLine(), out max);
+} while (max < 100 || max > 999);
+}
+int[] array = CreateRandomArray(num, min, max);
+
+ShowArray(array);
+
+int count = 0;
+
+for ( int i = 0; i < array.Length; i++)
+{
+if (array[i] % 2 == 0)
+count++;
+}
+Console.WriteLine($"Summ of even numbers in the array is: {count}");
